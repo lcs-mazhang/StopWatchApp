4a1a389855c810b14ea6bd666f58dadf622ce0b1
Matthew Zhang <gzhang@lcs.on.ca>
2023-11-03 09:46:16 -0400

    Initial Commit

---
+++ StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by GengYu Zhang on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
