496f57d60d104b71337f3c5c61ea12480b45bbd0
Matthew Zhang <gzhang@lcs.on.ca>
2023-11-06 06:46:45 -0500

    Creating the time and the reset button

--- StopWatch/ContentView.swift
+++ StopWatch/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+        
+        // First layer (backdround)
+            Color.black
+                .ignoresSafeArea()
+            
+            // Second layer (reset of interface)
+            VStack {
+                Text("00:09.96")
+                    .font(Font.system(size: 90, weight: .thin))
+                    .foregroundColor(.white)
+                
+                // Creat a circular button
+                ZStack {
+                    
+                    // from bottom 1 layer
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 100)
+                    
+                    // from bottom 2 layer
+                    Circle()
+                        .foregroundColor(.black)
+                        .frame(width:93)
+                    
+                    // from bottom 3 layer
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width:89)
+                    
+                    // Fourth layer
+                    Text("Reset")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                    
+                }
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
