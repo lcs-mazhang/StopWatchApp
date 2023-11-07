ddac306e68c22516d66da615d4202eb444ec809f
Matthew Zhang <gzhang@lcs.on.ca>
2023-11-07 14:27:30 -0500

    Adding tags

--- StopWatch/StopWatchApp.swift
+++ StopWatch/StopWatchApp.swift
@@ -12,29 +12,35 @@ struct StopWatchApp: App {
     var body: some Scene {
         WindowGroup {
 
-            TabView {
+            TabView(selection: Binding.constant(3)) {
+                
                 Text("World Clock")
                     .tabItem {
                         Image(systemName: "globe")
                         Text("World Clock")
                     }
+                    .tag(1)
                 
                 Text("Alarm")
                     .tabItem {
                         Image(systemName: "alarm.fill")
                         Text("World Clock")
                     }
+                    .tag(2)
+                
                 ContentView()
                     .tabItem {
                         Image(systemName: "stopwatch.fill")
                         Text("Stopwatch")
                     }
+                    .tag(3)
                 
                 Text("Timer")
                     .tabItem {
                         Image(systemName: "timer")
                         Text("Timer")
                     }
+                    .tag(4)
                 
             }
             // current tab color
