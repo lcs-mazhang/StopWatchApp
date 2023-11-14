352e91c8ca2f0ee6e0659592dd6d0b2d165dce29
Matthew Zhang <gzhang@lcs.on.ca>
2023-11-14 13:05:35 -0500

    adding spacer on top to fix the werid position and finished up the group

--- StopWatch/ContentView.swift
+++ StopWatch/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         ZStack {
         
         // First layer (backdround)
@@ -17,6 +18,9 @@ struct ContentView: View {
             
             // Second layer (reset of interface)
             VStack {
+                
+                Spacer()
+                
                 Text("00:09.96")
                     .font(Font.system(size: 90, weight: .thin))
                     .foregroundColor(.white)
@@ -29,6 +33,52 @@ struct ContentView: View {
                     
                     CircleButtonView(buttonColor: .darkGreen, label: "start", labelColor: .green)
                 }
+                
+                // List of times
+                
+                
+                List {
+                    
+                    Group {
+                        
+                        HStack{
+                            Text("Lap 5")
+                            Spacer()
+                            Text("00:00.98")
+                        }
+                        HStack{
+                            Text("Lap 4")
+                                .foregroundColor(.red)
+                            Spacer()
+                            Text("00:04.08")
+                                .foregroundColor(.red)
+                        }
+                        HStack {
+                            Text("Lap 3")
+                                .foregroundColor(.green)
+                            Spacer()
+                            Text("00:00:96")
+                                .foregroundColor(.green)
+                        }
+                        HStack{
+                            Text("Lap 2")
+                            Spacer()
+                            Text("00:02.76")
+                        }
+                        HStack{
+                            Text("Lap 1")
+                            Spacer()
+                            Text("00:01.16")
+                        }
+                    }
+                    // Remove inset from list items
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    
+                }
+                // Set the amount of vertical height we want this list to take up
+                .frame(height: 300)
+                // Adjust list style to match design
+                .listStyle(.plain)
                 
             }
             .padding()
